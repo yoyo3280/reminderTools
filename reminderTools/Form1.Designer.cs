222df69642c5a6280b65b0b3bcf3107dae27a6db
yoyo3280 <yoyo_407@hotmail.com>
2014-07-23 14:12:10 +0800

    add project

---
+++ reminderTools/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace reminderTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "提示工具";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(95, 26);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.label13);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 21);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(452, 195);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "提示信息";
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("华文新魏", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label13.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label13.Location = new System.Drawing.Point(7, 16);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(439, 23);
+            this.label13.TabIndex = 1;
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(446, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.label17);
+            this.tabPage1.Controls.Add(this.label16);
+            this.tabPage1.Controls.Add(this.label15);
+            this.tabPage1.Controls.Add(this.label14);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Location = new System.Drawing.Point(4, 21);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(452, 195);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "说明";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(44, 146);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(263, 12);
+            this.label17.TabIndex = 15;
+            this.label17.Text = "下午好，累了就发发呆吧.停下来听首歌吧。。。";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(4, 146);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(35, 12);
+            this.label16.TabIndex = 14;
+            this.label16.Text = "16:00";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(46, 31);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(233, 12);
+            this.label15.TabIndex = 13;
+            this.label15.Text = "喝杯水抖擞精神，开始下半场的工作。。。";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(3, 30);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(35, 12);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "10:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "9:00";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(44, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "每个醒后的早晨都是一件礼物，那个请打水去。。。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "午后的小憩,偷得片刻惬意,换来百倍抖擞。。。";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(44, 170);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(149, 12);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "一天中最开心的时刻。。。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "11:00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(4, 170);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 12);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "17:00";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(46, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "还在忙么，别忘了给自己一个微笑。。。";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(44, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(185, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "今天运动了没？打字不算哦。。。";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(44, 76);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(353, 12);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "工作再忙，午餐可不能少，午饭要好好吃哇..幸福是吃到饱。。。";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "15:00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 76);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 12);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "11:40";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "14:00";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(460, 220);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 221);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "提示工具";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ reminderTools/Form1.cs
@@ -0,0 +1,343 @@
+﻿/********************************************************************************
+** 作者：		yoyo
+** 创始时间：	2012-3-17
+*********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace reminderTools
+{
+    public partial class Form1 : Form
+    {
+        // 显示的图标 
+        private Icon ico1 = Properties.Resources.Icon1;
+
+        private Image image1 = Properties.Resources.Image1;
+
+        private Image image2 = Properties.Resources.Image2;
+
+        private Image image3 = Properties.Resources.Image3;
+
+        private Image image4 = Properties.Resources.Image4;
+
+        private Image image5 = Properties.Resources.Image5;
+
+        private Image image6 = Properties.Resources.Image6;
+
+        private Image image7 = Properties.Resources.Image7;
+
+        private Image image8 = Properties.Resources.Image8;
+
+        System.Timers.Timer t;
+
+        // 刷新间隔
+        int refreshTime = 1000;
+
+        Boolean flg = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.TopMost = true;
+            this.Icon = ico1;
+            SetTimer();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Icon = ico1;
+
+            this.notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+
+            const int WM_SYSCOMMAND = 0x0112;
+
+            const int SC_CLOSE = 0xF060;
+
+            if (m.Msg == WM_SYSCOMMAND && (int)m.WParam == SC_CLOSE)
+            { //捕捉关闭窗体消息       
+
+                //   User   clicked   close   button       
+
+                this.WindowState = FormWindowState.Minimized;
+
+                return;
+
+            }
+
+            base.WndProc(ref    m);
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.notifyIcon1.Icon = ico1;
+
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+
+            this.Activate();
+
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+
+            this.Close();
+
+            this.Dispose();
+
+            Application.Exit();
+        }
+
+        private void SetTimer()
+        {
+            // 实例化Timer类
+            t = new System.Timers.Timer();
+
+            t.Elapsed += new System.Timers.ElapsedEventHandler(theout);
+
+            t.SynchronizingObject = this;
+
+            t.AutoReset = true;
+
+            t.Interval = refreshTime;
+
+            t.Enabled = true;
+        }
+
+        private void theout(object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (!flg) 
+            {
+                string longTime = DateTime.Now.ToLongTimeString();
+
+                if ("00".Equals(longTime.Split(':')[2]))
+                {
+                    refreshTime = 60000;
+
+                    flg = true;
+
+                    t.Interval = refreshTime;
+                }
+            }
+            
+            string time = DateTime.Now.ToShortTimeString();
+
+            if ("9:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image1;
+
+                this.label13.Text = "每个醒后的早晨都是一件礼物，那个请打水去。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("10:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image2;
+
+                this.label13.Text = "喝杯水抖擞精神，开始下半场的工作。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("11:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image3;
+
+                this.label13.Text = "还在忙么，别忘了给自己一个微笑。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("11:40".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image4;
+
+                this.label13.Text = "工作再忙，午餐可不能少，午饭要好好吃哇..幸福是吃到饱。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("14:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image5;
+
+                this.label13.Text = "午后的小憩,偷得片刻惬意,换来百倍抖擞。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("15:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image6;
+
+                this.label13.Text = "今天运动了没？打字不算哦。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("16:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 255);
+
+                tabControl1.Size = new Size(460, 220);
+
+                pictureBox1.Image = image7;
+
+                this.label13.Text = "下午好，累了就发发呆吧.停下来听首歌吧。。。";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+            else if ("17:00".Equals(time))
+            {
+                refreshTime = 60000;
+
+                t.Interval = refreshTime;
+
+                flg = true;
+
+                this.Size = new Size(470, 435);
+
+                tabControl1.Size = new Size(460, 400);
+           
+                pictureBox1.Image = image8;
+
+                this.label13.Text = "";
+
+                this.tabControl1.SelectedIndex = 1;
+
+                this.Show();
+
+                this.WindowState = FormWindowState.Normal;
+
+                this.Activate();
+            }
+        }
+    }
+}
